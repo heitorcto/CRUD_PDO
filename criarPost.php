004bbc72284164969e3fda672fe3a5246bf10175
heitor <heitorcasella@gmail.com>
2022-01-29 22:25:07 -0300

    Att Fórum

--- criarPost.php
+++ criarPost.php
@@ -76,23 +76,7 @@ echo $usuario_logado; */
 
                      <!-- POST -->
                     <div class="mt-3 p-3 border-bottom">
-                        <!-- NOME DO USUÁRIO -->
-                        <div class="mb-2">
-                            <img src="imagens/UserDefault.png" class="rounded-circle" width="40" height="40">
-                            Nome
-                        </div>
-                        <!-- DESCRIÇÃO -->
-                        <div class="p-1">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </div>
-                        <!-- RESPONDER -->
-                        <div class="mt-2">
-                            <a href="">
-                                Responder <i class="far fa-comment-dots"></i>
-                            </a>
-                            <!-- ÍCONE DA LINGUAGEM -->
-                            <i class="ms-3 devicon-css3-plain" style="font-size:20px;"></i>
-                        </div>
+                        
                     </div>
 
                     <!-- CASO NÃO TENHA POSTS -->
