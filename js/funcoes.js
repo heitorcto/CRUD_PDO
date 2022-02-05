a9e179a30cc8f6d05ee5f3bf1d29b5cddf4b07ca
heitor <heitorcasella@gmail.com>
2022-02-05 18:40:01 -0300

    Efetuando testes

--- js/funcoes.js
+++ js/funcoes.js
@@ -5,3 +5,39 @@ function cadastro(){
         document.querySelector("form").submit();
     }, 1500)
 }
+
+
+// FUNÇÃO QUE EMITE UM GRÁFICO
+var ling1 = document.getElementById("ling1").value;
+var ling2 = document.getElementById("ling2").value;
+console.log(ling1)
+var linguagens = [ling1, ling2];
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: linguagens,
+        datasets: [{
+            label: 'Discussões',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
--- topLinguagens.php
+++ topLinguagens.php
@@ -3,55 +3,8 @@
     include("dbConnect.php");
     require_once __DIR__ . '/vendor/autoload.php';
 
-    // DEFININDO TIMEZONE PARA BRASIL - SP
-    date_default_timezone_set('America/Sao_Paulo');
-
-    use Post\Postagem;
-
     // CHAMANDO A SESSÃO
     session_start();
-
-    $postagem = new Postagem;
-
-    // VERIFICANDO POST PARA CRIAÇÃO DE UMA DISCUSSÃO NO FÓRUM
-    if(isset($_POST['descricao']) && (isset($_POST['codigo']) || isset($_POST['linguagem'])))
-    { 
-        // INSERINDO VALORES COM POST
-        $postagem->setUser_id($_SESSION['user']);
-        $postagem->setDescricao_post($_POST['descricao']);
-        $postagem->setCodigo_post($_POST['codigo']);
-        $postagem->setLinguagem_post($_POST['linguagem']);
-
-        // PASSANDO VALORES DO POST PARA AS VARIÁVEIS E EXECUTANDO O INSERT
-        $user_logado = $postagem->getUser_id();
-        $descricao = $postagem->getDescricao_post();
-        $codigo = $postagem->getCodigo_post();
-        $linguagem = $postagem->getLinguagem_post();
-        $data = date("d/m/Y");
-        $hora = date("H:i");
-
-        // INSERINDO NO BANCO COM VERIFICAÇÃO DE TRY CATCH
-        try{
-
-            // PREPARANDO O SQL E SEU PARÂMETRO
-            $inserir_postagem = $conn->prepare("INSERT INTO postagem (user_post_nome, descricao_post, codigo_post, linguagem_post, data_post, hora_post) VALUES (:user_post_nome, :descricao_post, :codigo_post, :linguagem_post, :data_post, :hora_post)");
-            $inserir_postagem->bindParam('user_post_nome', $user_logado);
-            $inserir_postagem->bindParam('descricao_post', $descricao);
-            $inserir_postagem->bindParam('codigo_post', $codigo);
-            $inserir_postagem->bindParam('linguagem_post', $linguagem);
-            $inserir_postagem->bindParam('data_post', $data);
-            $inserir_postagem->bindParam('hora_post', $hora);
-
-            // EXECUTANDO O INSERT
-            $inserir_postagem->execute();
-
-        } catch(PDOException $e) {
-
-            // EMITINDO ERRO QUANDO O POST NÃO FOI INSERIDO
-            echo $e->getMessage();
-
-        }
-    }
 
 ?>
 
@@ -72,18 +25,19 @@
         <!-- FONT AWESOME -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
         
-        <!-- JS PRÓPRIO -->
-        <script src="js/funcoes.js"></script>
-        
         <!-- CSS PRÓPRIO -->
         <link href="estilos/estiloGeral.css" rel="stylesheet">
         
         <!-- FAVICON -->
         <link rel="icon" href="imagens/iconeProjeto.png" type="image/png">
 
+        <!-- CHARTS.JS -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
         <!-- TÍTULO DA PÁGINA -->
         <title>Fórum - Projeto CRUD</title>
     </head>
+    
     <body>
         <div class="container">
             <div class="row">
@@ -94,7 +48,7 @@
                             <img class="icone_zoom" src="imagens/iconeProjeto.png">
                         </div>
                     </a>
-                    <a href="topLinguagens.php">
+                    <a href="">
                        <div class="d-flex justify-content-center mt-2 mb-2 btn btn-king">
                             TOP 5
                         </div>
@@ -124,49 +78,17 @@
                     <div class="mt-3 p-3 border-bottom">
                         <!-- NOME DO USUÁRIO -->
                         <div class="d-flex justify-content-center mb-4">
-                            Olá, <?php echo $_SESSION['user']; ?>, compartilhe suas experiências ou dúvidas.
-                        </div>
-                        <div class="d-flex justify-content-center mb-2">
-                            <a href="criarPost.php" type="button" class="btn btn-purple ms-3">Criar Post</a>
+                            Veja as linguagens mais discutidas no fórum.
                         </div>
                     </div>
 
-                    <?php 
-                        // LISTAR TODOS OS POSTS
-                        $listagem_postagem = $conn->prepare("SELECT * FROM postagem ORDER BY id DESC");
-                        $listagem_postagem->execute();
-
-                        while($post_info = $listagem_postagem->fetch(PDO::FETCH_ASSOC))
-                        { 
-                    ?>
-                            <!-- POST -->
-                            <div class="mt-3 p-3 border-bottom">
-                                <!-- NOME DO USUÁRIO -->
-                                <div class="mb-2">
-                                    <img src="imagens/UserDefault.png" class="rounded-circle" width="40" height="40">
-                                    <span class="ms-1 fs-6 fw-bold"><?php echo $post_info['user_post_nome']; ?></span>
-                                    <span class="fs-6 fst-italic float-end"><?php echo $post_info['data_post']." às ".$post_info['hora_post']; ?></span>
-                                </div>
-                                <!-- DESCRIÇÃO -->
-                                <div class="p-1">
-                                    <?php echo $post_info['descricao_post']; ?>
-                                </div>
-                                <!-- RESPONDER -->
-                                <div class="mt-2">
-                                    <a class="btn btn-purple" href="">
-                                        Ver Mais <i class="ms-1 fas fa-plus"></i>
-                                    </a>
-                                    <!-- ÍCONE DA LINGUAGEM -->
-                                    <a href="">
-                                        <div class="btn btn-purple">
-                                            <?php echo $post_info['linguagem_post']; ?>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                    <?php 
-                        }
-                    ?>
+                    <!-- SETANDO TOP 5 LINGUAGENS -->
+                    <canvas id="myChart" width="400" height="400"></canvas>
+                    <input type="hidden" id="ling1" value="PHP"></input>
+                    <input type="hidden" id="ling2" value="CSS"></input>
+                    <input type="hidden" id="ling3" value=""></input>
+                    <input type="hidden" id="ling4" value=""></input>
+                    <input type="hidden" id="ling5" value=""></input>
                 </div>
 
                 <!-- USUÁRIO E OUTRAS INFORMAÇÕES -->
@@ -201,3 +123,6 @@
         </div>
     </body>
 </html>
+
+<!-- JS PRÓPRIO -->
+<script src="js/funcoes.js"></script>
